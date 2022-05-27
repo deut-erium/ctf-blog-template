098ba4fa4a3919c85158bf1d61c375bbf7f32133
deut-erium <himanshuthesheoran@gmail.com>
2022-05-28 02:51:13 +0530

    new version Sat May 28 02:51:13 IST 2022

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Contributions",'url':"/ctf-writeups-2021/2021-04-08-contributions"},{'title':"Welcome",'url':"/ctf-writeups-2021/2021-04-08-welcome"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Contributions",'url':"/ctf-blog-template/2021-04-08-contributions"},{'title':"Welcome",'url':"/ctf-blog-template/2021-04-08-welcome"}]};
